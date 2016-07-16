9d49aa81020e6ecce45fbf2ad1db08969341abc9
joshyuan360 <joshua.gyuan@gmail.com>
2016-07-16 13:09:49 -0400

    change Button1 properties

--- app/src/main/java/com/joshuayuan/eartraining/Button1.java
+++ app/src/main/java/com/joshuayuan/eartraining/Button1.java
@@ -11,6 +11,7 @@
 package com.joshuayuan.eartraining;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.Button;
@@ -21,7 +22,9 @@ import android.widget.Button;
 public class Button1 extends Button {
     public Button1(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/GIL.TTF"));
-        this.setTextSize(12);
+        setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/GIL.TTF"));
+        setTextSize(12);
+        setTextColor(Color.parseColor("#FFFFFF"));
+        setBackgroundResource(R.drawable.custom_button);
     }
 }
