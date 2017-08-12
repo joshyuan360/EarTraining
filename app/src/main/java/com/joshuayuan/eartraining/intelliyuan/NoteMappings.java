a312d9b56fb319c9073768de70e12444bc1bd4bc
Joshua Yuan <joshua.gyuan@gmail.com>
2017-08-11 23:07:12 -0400

    hotfix for aug issue

--- app/src/main/java/com/joshuayuan/eartraining/intelliyuan/NoteMappings.java
+++ app/src/main/java/com/joshuayuan/eartraining/intelliyuan/NoteMappings.java
@@ -12,11 +12,6 @@ package com.joshuayuan.eartraining.intelliyuan;
 
 import com.joshuayuan.eartraining.R;
 
-/**
- * Used in multiple classes within the com.joshuayuan package.
- *
- * @author Joshua Yuan
- */
 public class NoteMappings {
     public static int MAX_NOTE = 28;
     public static int MIN_NOTE = -11;
@@ -28,47 +23,90 @@ public class NoteMappings {
      * @return The integer ID of the note specified by the parameter.
      */
     public static int getResourceId(int note) {
-        if (note == 1) return R.raw.n1;
-        if (note == 2) return R.raw.n2;
-        if (note == 3) return R.raw.n3;
-        if (note == 4) return R.raw.n4;
-        if (note == 5) return R.raw.n5;
-        if (note == 6) return R.raw.n6;
-        if (note == 7) return R.raw.n7;
-        if (note == 8) return R.raw.n8;
-        if (note == 9) return R.raw.n9;
-        if (note == 10) return R.raw.n10;
-        if (note == 11) return R.raw.n11;
-        if (note == 12) return R.raw.n12;
-        if (note == 13) return R.raw.n13;
-        if (note == 14) return R.raw.n14;
-        if (note == 15) return R.raw.n15;
-        if (note == 16) return R.raw.n16;
-        if (note == 17) return R.raw.n17;
-        if (note == 18) return R.raw.n18;
-        if (note == 19) return R.raw.n19;
-        if (note == 20) return R.raw.n20;
-        if (note == 21) return R.raw.n21;
-        if (note == 22) return R.raw.n22;
-        if (note == 23) return R.raw.n23;
-        if (note == 24) return R.raw.n24;
-        if (note == 25) return R.raw.n25;
-        if (note == 26) return R.raw.n26;
-        if (note == 27) return R.raw.n27;
-        if (note == 28) return R.raw.n28;
-        if (note == 0) return R.raw.low0;
-        if (note == -1) return R.raw.low1;
-        if (note == -2) return R.raw.low2;
-        if (note == -3) return R.raw.low3;
-        if (note == -4) return R.raw.low4;
-        if (note == -5) return R.raw.low5;
-        if (note == -6) return R.raw.low6;
-        if (note == -7) return R.raw.low7;
-        if (note == -8) return R.raw.low8;
-        if (note == -9) return R.raw.low9;
-        if (note == -10) return R.raw.low10;
-        if (note == -11) return R.raw.low11;
-        return -1000;
+        switch (note) {
+            case 1:
+                return R.raw.n1;
+            case 2:
+                return R.raw.n2;
+            case 3:
+                return R.raw.n3;
+            case 4:
+                return R.raw.n4;
+            case 5:
+                return R.raw.n5;
+            case 6:
+                return R.raw.n6;
+            case 7:
+                return R.raw.n7;
+            case 8:
+                return R.raw.n8;
+            case 9:
+                return R.raw.n9;
+            case 10:
+                return R.raw.n10;
+            case 11:
+                return R.raw.n11;
+            case 12:
+                return R.raw.n12;
+            case 13:
+                return R.raw.n13;
+            case 14:
+                return R.raw.n14;
+            case 15:
+                return R.raw.n15;
+            case 16:
+                return R.raw.n16;
+            case 17:
+                return R.raw.n17;
+            case 18:
+                return R.raw.n18;
+            case 19:
+                return R.raw.n19;
+            case 20:
+                return R.raw.n20;
+            case 21:
+                return R.raw.n21;
+            case 22:
+                return R.raw.n22;
+            case 23:
+                return R.raw.n23;
+            case 24:
+                return R.raw.n24;
+            case 25:
+                return R.raw.n25;
+            case 26:
+                return R.raw.n26;
+            case 27:
+                return R.raw.n27;
+            case 28:
+                return R.raw.n28;
+            case 0:
+                return R.raw.low0;
+            case -1:
+                return R.raw.low1;
+            case -2:
+                return R.raw.low2;
+            case -3:
+                return R.raw.low3;
+            case -4:
+                return R.raw.low4;
+            case -5:
+                return R.raw.low5;
+            case -6:
+                return R.raw.low6;
+            case -7:
+                return R.raw.low7;
+            case -8:
+                return R.raw.low8;
+            case -9:
+                return R.raw.low9;
+            case -10:
+                return R.raw.low10;
+            case -11:
+                return R.raw.low11;
+            default:
+                return -1000;
+        }
     }
 
 }
--- app/src/main/java/com/joshuayuan/eartraining/syllabus/PianoSyllabus.java
+++ app/src/main/java/com/joshuayuan/eartraining/syllabus/PianoSyllabus.java
@@ -85,7 +85,7 @@ public class PianoSyllabus {
 
         levelToIntervalMap.append(7, new String[] { "Minor Second", "Major Second", "Minor Third", "Major Third", "Perfect Fourth", "Perfect Fifth", "Minor Sixth", "Major Sixth", "Minor Seventh", "Major Seventh", "Perfect Octave" });
         levelToIntervalTypeMap.append(7, "4");
-        levelToChordsMap.append(7, new String[] { "Dom 7 Root Pos", "Dim 7 none", "Augmented Triad" });
+        levelToChordsMap.append(7, new String[] { "Dom 7 Root Pos", "Dim 7 none", "Aug none" });
         levelToChordTypeMap.append(7, "1");
         levelToCadencesMap.append(7, new String [] { });
         levelToProgressionsMap.append(7, new String[] { });
@@ -94,7 +94,7 @@ public class PianoSyllabus {
 
         levelToIntervalMap.append(8, new String[] { "Minor Second", "Major Second", "Minor Third", "Major Third", "Perfect Fourth", "Aug 4", "Perfect Fifth", "Minor Sixth", "Major Sixth", "Minor Seventh", "Major Seventh", "Perfect Octave" });
         levelToIntervalTypeMap.append(8, "4");
-        levelToChordsMap.append(8, new String[] { "Dom 7 Root Pos", "Dim 7 none", "Augmented Triad" });
+        levelToChordsMap.append(8, new String[] { "Dom 7 Root Pos", "Dim 7 none", "Aug none" });
         levelToChordTypeMap.append(8, "1");
         levelToCadencesMap.append(8, new String [] { });
         levelToProgressionsMap.append(8, new String[] { "six" });
@@ -103,7 +103,7 @@ public class PianoSyllabus {
 
         levelToIntervalMap.append(9, new String[] { "Minor Second", "Major Second", "Minor Third", "Major Third", "Perfect Fourth", "Aug 4", "Perfect Fifth", "Minor Sixth", "Major Sixth", "Minor Seventh", "Major Seventh", "Perfect Octave" });
         levelToIntervalTypeMap.append(9, "4");
-        levelToChordsMap.append(9, new String[] { "Major 1st Inv", "Minor 1st Inv", "Dom 7 Root Pos", "Dim 7 none", "Augmented Triad" });
+        levelToChordsMap.append(9, new String[] { "Major 1st Inv", "Minor 1st Inv", "Dom 7 Root Pos", "Dim 7 none", "Aug none" });
         levelToChordTypeMap.append(9, "1");
         levelToCadencesMap.append(9, new String [] { });
         levelToProgressionsMap.append(9, new String[] { "six" });
@@ -112,7 +112,7 @@ public class PianoSyllabus {
 
         levelToIntervalMap.append(10, new String[] { "Minor Second", "Major Second", "Minor Third", "Major Third", "Perfect Fourth", "Aug 4", "Perfect Fifth", "Minor Sixth", "Major Sixth", "Minor Seventh", "Major Seventh", "Perfect Octave", "Major Ninth", "Minor Ninth" });
         levelToIntervalTypeMap.append(10, "4");
-        levelToChordsMap.append(10, new String[] { "Major 1st Inv", "Minor 1st Inv", "Dom 7 Root Pos", "Dim 7 none", "Augmented Triad", "Major 7 Root Pos", "Minor 7 Root Pos" }); // todo: add four note chords (release 6.3)
+        levelToChordsMap.append(10, new String[] { "Major 1st Inv", "Minor 1st Inv", "Dom 7 Root Pos", "Dim 7 none", "Aug none", "Major 7 Root Pos", "Minor 7 Root Pos" }); // todo: add four note chords (release 6.3)
         levelToChordTypeMap.append(10, "1");
         levelToCadencesMap.append(10, new String [] { });
         levelToProgressionsMap.append(10, new String[] { "six", "cadential"});
